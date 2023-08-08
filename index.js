b75c58a96914f3144922497d0a357487a22b0342
tiagolimar <tiagolima.r2@gmail.com>
2023-08-08 12:53:26 -0300

    adição de participantes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-const names=["TIAGO","MATHEUS","VINÍCIUS","JANSEN","FERNANDO"]
+const names=["TIAGO","MATHEUS","VINÍCIUS","JANSEN","FERNANDO","WESCLEY","GEORGE"]
 
 const create_opts = ()=>{
     elementsHtml = ''
@@ -33,4 +33,22 @@ const pickWinner = () => {
 
     const winnerElement = document.getElementById("winner");
     winnerElement.innerHTML = `<center>${winner}</center>`;
+}
+
+const getId = () => {
+    const checkboxes = document.getElementsByClassName("form-check-input")
+    return checkboxes.length
+}
+
+const adicionar = () => {
+    if (novo_nome.value){
+        let name = novo_nome.value.toUpperCase();
+        let id = getId()
+        names_container.innerHTML += `<div class="form-check">
+        <input class="form-check-input" type="checkbox" value="${name}" id="name${id}" checked>
+        <label class="form-check-label" for="name${id}">
+          ${name}
+        </label>
+        </div>`
+    }
 }
